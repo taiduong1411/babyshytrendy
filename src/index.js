ca64a8f73dec34134a9cd85c9af4d838f1e665a7
DuongTrongTai <duongtai141120@gmail.com>
2022-10-02 17:27:18 +0700

    update port

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const ProductsRouter = require('./routers/ProductsRouter');
 const AdminRouter = require('./routers/AdminRouter');
 // APIs
 const AdminAPI = require('./API/AdminAPI');
-const port = 3000;
+const port = process.env.PORT || 3000;
 database.connect();
 //  config
 app.set('view engine', 'hbs');
